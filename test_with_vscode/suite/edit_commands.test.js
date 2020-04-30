8f9065d3835b0358eef9534b5eff8cebb1773072
tshino <tshino.ssr@gmail.com>
2020-05-01 00:35:21 +0900

    Add test for edit_commands

--- test_with_vscode/suite/edit_commands.test.js
+++ test_with_vscode/suite/edit_commands.test.js
@@ -6,7 +6,7 @@ const mode_handler = require("./../../src/mode_handler.js");
 const edit_commands = require("./../../src/edit_commands.js");
 
 describe('EditHandler', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+    vscode.window.showInformationMessage('Started test for EditHandler.');
     const mode = mode_handler.ModeHandler();
     const editHandler = edit_commands.EditHandler(mode);
 
@@ -20,7 +20,7 @@ describe('EditHandler', () => {
                 'fghij\n' +
                 '\n' +
                 '12345\n' +
-                '67890\n'
+                '67890' // <= no new line
             )
         });
     });
@@ -82,6 +82,77 @@ describe('EditHandler', () => {
             assert.equal(mode.inSelection(), false);
             assert.equal(textEditor.selections.length, 1);
             assert(textEditor.selections[0].isEqual( new vscode.Selection(1, 0, 1, 0) ));
+        });
+    });
+    describe('readText', () => {
+        it('should extract text from document (single range)', () => {
+            mode.initialize(textEditor);
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(1, 3, 1, 7) ]),
+                '4567'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(0, 0, 0, 10) ]),
+                '1234567890'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(0, 0, 1, 0) ]),
+                '1234567890\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(0, 5, 1, 5) ]),
+                '67890\n12345'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(0, 10, 1, 0) ]),
+                '\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(3, 0, 6, 0) ]),
+                'fghij\n\n12345\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(6, 0, 7, 0) ]),
+                '67890' // no new line (end of document)
+            );
+        });
+        it('should extract text from document (empty range)', () => {
+            mode.initialize(textEditor);
+            assert.equal(
+                editHandler.readText(textEditor, [ new vscode.Range(1, 3, 1, 3) ]),
+                ''
+            );
+        });
+        it('should extract text from document (multi range)', () => {
+            mode.initialize(textEditor);
+            assert.equal(
+                editHandler.readText(textEditor, [
+                    new vscode.Range(0, 5, 0, 8),
+                    new vscode.Range(1, 5, 1, 8)
+                ]),
+                '678\n678\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [
+                    new vscode.Range(0, 0, 0, 10),
+                    new vscode.Range(1, 0, 1, 10)
+                ]),
+                '1234567890\n1234567890\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [
+                    new vscode.Range(3, 0, 3, 5),
+                    new vscode.Range(2, 0, 2, 5)
+                ]),
+                'abcde\nfghij\n'
+            );
+            assert.equal(
+                editHandler.readText(textEditor, [
+                    new vscode.Range(5, 0, 5, 5),
+                    new vscode.Range(6, 0, 6, 5)
+                ]),
+                '12345\n67890\n'
+            );
         });
     });
 });
