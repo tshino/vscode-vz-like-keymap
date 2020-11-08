808a9a4d08cc8c688112667c86047bf0e2094fea
tshino <tshino.ssr@gmail.com>
2020-11-08 20:18:11 +0900

    Small refactoring

--- src/extension.js
+++ src/extension.js
@@ -4,15 +4,20 @@ const mode_handler = require("./mode_handler.js");
 const cursor_style = require("./cursor_style.js");
 const edit_commands = require("./edit_commands.js");
 const cursor_commands = require("./cursor_commands.js");
+const search_commands = require("./search_commands.js");
 
 function activate(context) {
     const mode = mode_handler.getInstance();
     const cursor_style_controller = cursor_style.CursorStyleController();
     const editHandler = edit_commands.getInstance();
     const cursorHandler = cursor_commands.getInstance();
+    const searchHandler = search_commands.getInstance();
     const modeIndicator = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 90);
+
     editHandler.registerCommands(context);
     cursorHandler.registerCommands(context);
+    searchHandler.registerCommands(context);
+
     mode.onStartSelection(function(textEditor) {
         vscode.commands.executeCommand('setContext', 'vz.inSelectionMode', true);
         modeIndicator.text = "[B]";
@@ -24,6 +29,7 @@ function activate(context) {
         modeIndicator.hide();
         cursor_style_controller.resetSelection(textEditor);
     });
+
     if (vscode.window.activeTextEditor) {
         cursor_style_controller.initialize();
         mode.initialize(vscode.window.activeTextEditor);
@@ -37,58 +43,6 @@ function activate(context) {
             }
         })
     );
-    const registerTextEditorCommand = function(name, func) {
-        context.subscriptions.push(
-            vscode.commands.registerTextEditorCommand('vz.' + name, func)
-        );
-    };
-    const exec = function(commands, index = 0) {
-        if (typeof commands === 'string') {
-            commands = [ commands ];
-        }
-        let res = vscode.commands.executeCommand(commands[index]);
-        if (index + 1 < commands.length) {
-            res.then(function() { exec(commands, index + 1); });
-        }
-    };
-
-    registerTextEditorCommand('find', function(_textEditor, _edit) {
-        exec(['closeFindWidget', 'actions.find']);
-    });
-    const isCursorAtEndOfLine = function(textEditor) {
-        let cursor = textEditor.selection.active;
-        let lineLen = textEditor.document.lineAt(cursor.line).range.end.character;
-        return lineLen <= cursor.character;
-    };
-    registerTextEditorCommand('selectWordToFind', function(textEditor, _edit) {
-        if (textEditor.selection.isEmpty && !isCursorAtEndOfLine(textEditor)) {
-            exec(['cursorWordEndRightSelect', 'actions.find']);
-        } else {
-            exec(['actions.find']);
-        }
-    });
-    registerTextEditorCommand('expandWordToFind', function(textEditor, _edit) {
-        let sel = textEditor.selection;
-        if (1 < textEditor.selections.length || sel.anchor.line !== sel.active.line) {
-            return;
-        }
-        if (sel.anchor.character > sel.active.character) {
-            sel = new vscode.Selection(sel.active, sel.anchor);
-            textEditor.selection = sel;
-        }
-        if (isCursorAtEndOfLine(textEditor)) {
-            return;
-        }
-        exec(['cursorWordEndRightSelect', 'actions.find']);
-    });
-    registerTextEditorCommand('closeFindWidget', function(textEditor, _edit) {
-        textEditor.selection = new vscode.Selection(
-            textEditor.selection.start,
-            textEditor.selection.start
-        );
-        mode.resetSelection(textEditor);
-        exec(['closeFindWidget']);
-    });
 }
 exports.activate = activate;
 
---
+++ src/search_commands.js
@@ -0,0 +1,77 @@
+"use strict";
+const vscode = require("vscode");
+const mode_handler = require("./mode_handler.js");
+
+const exec = function(commands, index = 0) {
+    if (typeof commands === 'string') {
+        commands = [ commands ];
+    }
+    let res = vscode.commands.executeCommand(commands[index]);
+    if (index + 1 < commands.length) {
+        res.then(function() { exec(commands, index + 1); });
+    }
+};
+const registerTextEditorCommand = function(context, name, func) {
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand('vz.' + name, func)
+    );
+};
+
+const SearchHandler = function(modeHandler) {
+    const mode = modeHandler;
+
+    const find = function(_textEditor, _edit) {
+        exec(['closeFindWidget', 'actions.find']);
+    };
+    const isCursorAtEndOfLine = function(textEditor) {
+        let cursor = textEditor.selection.active;
+        let lineLen = textEditor.document.lineAt(cursor.line).range.end.character;
+        return lineLen <= cursor.character;
+    };
+    const selectWordToFind = function(textEditor, _edit) {
+        if (textEditor.selection.isEmpty && !isCursorAtEndOfLine(textEditor)) {
+            exec(['cursorWordEndRightSelect', 'actions.find']);
+        } else {
+            exec(['actions.find']);
+        }
+    };
+    const expandWordToFind = function(textEditor, _edit) {
+        let sel = textEditor.selection;
+        if (1 < textEditor.selections.length || sel.anchor.line !== sel.active.line) {
+            return;
+        }
+        if (sel.anchor.character > sel.active.character) {
+            sel = new vscode.Selection(sel.active, sel.anchor);
+            textEditor.selection = sel;
+        }
+        if (isCursorAtEndOfLine(textEditor)) {
+            return;
+        }
+        exec(['cursorWordEndRightSelect', 'actions.find']);
+    };
+    const closeFindWidget = function(textEditor, _edit) {
+        textEditor.selection = new vscode.Selection(
+            textEditor.selection.start,
+            textEditor.selection.start
+        );
+        mode.resetSelection(textEditor);
+        exec(['closeFindWidget']);
+    };
+
+    const registerCommands = function(context) {
+        registerTextEditorCommand(context, 'find', find);
+        registerTextEditorCommand(context, 'selectWordToFind', selectWordToFind);
+        registerTextEditorCommand(context, 'expandWordToFind', expandWordToFind);
+        registerTextEditorCommand(context, 'closeFindWidget', closeFindWidget);
+    };
+    return {
+        registerCommands
+    };
+};
+
+exports.SearchHandler = SearchHandler;
+
+const theInstance = SearchHandler(mode_handler.getInstance());
+exports.getInstance = function() {
+    return theInstance;
+};
