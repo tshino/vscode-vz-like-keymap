996417de64f0c6ec3228f01f6a23506cecb86735
tshino <tshino.ssr@gmail.com>
2021-09-18 21:04:26 +0900

    Add tests for command_util.js

--- test/command_util_test.js
+++ test/command_util_test.js
@@ -17,20 +17,61 @@ describe('CommandUtil', function() {
             CommandUtil.setCommandListener(null);
         });
         it('should return a function that calls given function', async () => {
-            let log = [];
+            const logs = [];
             const retval = CommandUtil.makeGuardedCommand(
                 'name',
                 function() {
-                    log.push('invoked');
+                    logs.push('invoked');
                 }
             );
             assert.strictEqual(typeof retval, 'function');
             await retval();
-            assert.deepStrictEqual(log, ['invoked']);
+            assert.deepStrictEqual(logs, ['invoked']);
         });
         // TODO: add more tests for makeGuardedCommand
     });
-    // TODO: add tests for setCommandListener
+    describe('setCommandListener', function() {
+        beforeEach(async () => {
+            CommandUtil.setCommandListener(null);
+        });
+        after(async () => {
+            CommandUtil.setCommandListener(null);
+        });
+        it('should set a function to observe what commands have been invoked', async () => {
+            const logs = [];
+            const cmd1 = CommandUtil.makeGuardedCommand('cmd1', function() {
+                logs.push('cmd1 invoked');
+            });
+            const cmd2 = CommandUtil.makeGuardedCommand('cmd2', function() {
+                logs.push('cmd2 invoked');
+            });
+
+            CommandUtil.setCommandListener(function(name, func) {
+                logs.push([name, func]);
+            });
+            await cmd2();
+            await cmd1();
+            await cmd2();
+
+            assert.deepStrictEqual(logs, [
+                [CommandUtil.CommandPrefix + 'cmd2', cmd2],
+                'cmd2 invoked',
+                [CommandUtil.CommandPrefix + 'cmd1', cmd1],
+                'cmd1 invoked',
+                [CommandUtil.CommandPrefix + 'cmd2', cmd2],
+                'cmd2 invoked'
+            ]);
+
+            logs.length = 0;
+            CommandUtil.setCommandListener(null);
+            await cmd1();
+            await cmd2();
+            assert.deepStrictEqual(logs, [
+                'cmd1 invoked',
+                'cmd2 invoked'
+            ]);
+        });
+    });
     // TODO: add tests for waitForEndOfGuardedCommand
     // TODO: add tests for makeRegisterTextEditorCommand
 });
